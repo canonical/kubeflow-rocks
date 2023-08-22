aed96e3a8e973b5d92a6cbe0fb6eceeab6c5f3e8
Ivan Chvets <ivan.chvets@canonical.com>
2023-08-22 15:27:06 -0400

    fix: updated pr according to code review
    Summary of changes:
    - Updated tox.ini to have more descriptive environment names.
    - Modified tox.ini to use requirements files.
    - Added requirements* files.
    - Updated test to catch errors and properly report failed tests.

--- notebook-controller/tests/test_rock.py
+++ notebook-controller/tests/test_rock.py
@@ -1,4 +1,4 @@
-# Copyright 2022 Canonical Ltd.
+# Copyright 2023 Canonical Ltd.
 # See LICENSE file for licensing details.
 #
 # Tests for required artifacts to be present in the ROCK image.
@@ -18,21 +18,29 @@ from pytest_operator.plugin import OpsTest
 
 @pytest.fixture()
 def rock_test_env():
-    """Yields a temporary directory and random docker container name, then cleans them up after."""
+    """Yelds random docker container name, then cleans it up after."""
     container_name = "".join([str(i) for i in random.choices(string.ascii_lowercase, k=8)])
     yield container_name
 
     try:
-        subprocess.run(["docker", "rm", container_name])
-    except Exception:
-        pass
+        subprocess.run(["docker", "rm", container_name], check=True, capture_output=True)
+    except Exception as error:
+        print(f"Failed to remove Docker container {container_name} error: {str(error)}")
+
 
 @pytest.mark.abort_on_fail
-def test_rock(ops_test: OpsTest, rock_test_env):
-    """Test rock."""
+def test_rock_artefacts(ops_test: OpsTest, rock_test_env):
+    """Test that all artefacts are present in the ROCK: binaries."""
     check_rock = CheckRock("rockcraft.yaml")
     container_name = rock_test_env
     LOCAL_ROCK_IMAGE = f"{check_rock.get_image_name()}:{check_rock.get_version()}"
 
     # verify that all artifacts are in correct locations
-    subprocess.run(["docker", "run", LOCAL_ROCK_IMAGE, "exec", "ls", "-la", "/manager"], check=True)
+    try:
+        subprocess.run(
+            ["docker", "run", "--name", container_name, LOCAL_ROCK_IMAGE, "exec", "ls", "-la", "/manager"],
+            check=True
+        )
+    except subprocess.CalledProcessError as error:
+        print(f"Failed to execute docker run for container {container_name} error: {str(error)}")
+        assert 0
