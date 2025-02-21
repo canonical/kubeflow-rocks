5feebd7f107f7dded1a7ac9cb33cbf51a8398011
Michal Hucko <hucko.miso@gmail.com>
2025-02-21 12:16:36 +0100

    feat: update `jupyter-scipy` rock for `1.10.0-rc` (#159)
    * feat: update scipy rock for 1.10.0-rc

--- jupyter-scipy/tests/imports.py
+++ jupyter-scipy/tests/imports.py
@@ -1,25 +1,18 @@
 #
 # This python script tests loading of required modules
 #
-# jupyter packages
+
+# Jupyter packages
 import jupyterlab
 import notebook
 import ipykernel
 
-# kubeflow packages
-# TO-DO verfiy proper kfp import. Upgrade mught be needed.
-#import kfp
-import kfp_server_api
-import kfserving
-
-# scipy packages
-# https://github.com/jupyter/docker-stacks/blob/master/scipy-notebook/Dockerfile
-# TO-DO verify how beautifulsoup4 is used/installed
-# /opt/conda/lib/python3.8/site-packages/beautifulsoup4-4.9.3.dist-info
-#import beautifulsoup4
+# SciPy ecosystem packages
+import altair
 import bokeh
+import bottleneck
+import brotli
 import cloudpickle
-import cython
 import dask
 import dill
 import h5py
@@ -29,29 +22,27 @@ import jupyterlab_git
 import matplotlib
 import numba
 import numexpr
+import openpyxl
 import pandas
 import patsy
-# TO-DO verfy how protobuf is installed
-# /opt/conda/lib/python3.8/site-packages/protobuf-3.17.3.dist-info
-#import protobuf
-# google contains protobuf, importing it instead
-import google
-# TO-DO verify how exactly scikit-image is installed
-# /opt/conda/lib/python3.8/site-packages/scikit_image-0.18.1.dist-info
-#import scikit_image
-# TO-DO verify how exactly scikit-learn is installed
-# /opt/conda/lib/python3.8/site-packages/scikit_learn-0.24.2.dist-info
-#import scikit_learn
 import scipy
 import seaborn
-# TO-DO verify how SQLAlchemy is installed
-# /opt/conda/lib/python3.8/site-packages/SQLAlchemy-1.4.18.dist-info
-#import SQLAlchemy
+import sqlalchemy
 import statsmodels
 import sympy
-import tables
+import tables  # pytables
 import vincent
 import xlrd
 
-# this string is expected by test script
+# Machine Learning & Data Science
+import sklearn  # scikit-learn
+
+# MLflow package
+import mlflow
+
+# OpenBLAS (Note: OpenBLAS itself is a C library, but NumPy uses it)
+import numpy as np
+import scipy.linalg  # Ensures OpenBLAS is properly linked
+
+# Test script success output
 print("PASSED")
--- jupyter-scipy/tests/test_access.py
+++ jupyter-scipy/tests/test_access.py
@@ -1,17 +1,13 @@
 #!/usr/bin/env python3
-# Copyright 2023 Canonical Ltd.
+# Copyright 2025 Canonical Ltd.
 # See LICENSE file for licensing details.
 #
 #
-from pathlib import Path
-
-import os
 import subprocess
-import time
 import requests
 import tenacity
-import yaml
 
+from charmed_kubeflow_chisme.rock import CheckRock
 
 @tenacity.retry(
 stop=tenacity.stop_after_attempt(5),
@@ -24,19 +20,18 @@ def check_notebook_server_up(url):
 
 def main():
     """Test running container and imports."""
-    rock = yaml.safe_load(Path("rockcraft.yaml").read_text())
-    name = rock["name"]
-    rock_version = rock["version"]
-    arch = list(rock["platforms"].keys())[0]
-    rock_image = f"{name}_{rock_version}_{arch}"
+    check_rock = CheckRock("rockcraft.yaml")
+    rock_image = check_rock.get_name()
+    rock_version = check_rock.get_version()
     LOCAL_ROCK_IMAGE = f"{rock_image}:{rock_version}"
     
     print(f"Running {LOCAL_ROCK_IMAGE}")
-    container_id = subprocess.run(["docker", "run", "-d", "-p", "8888:8888", LOCAL_ROCK_IMAGE], stdout=subprocess.PIPE).stdout.decode('utf-8')
+    NB_PREFIX_DIR="test"
+    container_id = subprocess.run(["docker", "run", "-d", "-p", "8888:8888", "-e", f"NB_PREFIX={NB_PREFIX_DIR}", LOCAL_ROCK_IMAGE], stdout=subprocess.PIPE).stdout.decode('utf-8')
     container_id = container_id[0:12]
 
     # Try to reach the notebook server
-    output = check_notebook_server_up("http://0.0.0.0:8888/lab")
+    output = check_notebook_server_up(f"http://0.0.0.0:8888/{NB_PREFIX_DIR}/lab")
 
     # cleanup
     subprocess.run(["docker", "stop", f"{container_id}"])
