9a7bded878999a7de978355810a7ce58d549c11a
Danylo Bereznenko <pepsiqqfanta@gmail.com>
2025-04-15 18:46:07 +0300

    Add `Rstudio-tidyverse` rock and tests (#193)
    * Rock Add
    
    * Rock Add
    
    * Rock Add
    
    * Rock Add
    
    * Rock Add
    
    * Rock Add
    
    * Rock Add
    
    * Rock Add
    
    * Rock Add
    
    * Rock Add
    
    * Rock Add
    
    Signed-off-by: pepsiqqq <pepsiqqfanta@gmail.com>
    
    * Rock Add
    
    Signed-off-by: pepsiqqq <pepsiqqfanta@gmail.com>
    
    * Rock Add
    
    * Rock Add
    
    * Rock update
    
    * Rock update
    
    * Rock update
    
    * Rock update
    
    * Rock update
    
    ---------
    
    Signed-off-by: pepsiqqq <pepsiqqfanta@gmail.com>

---
+++ rstudio-tidyverse/tests/test_rock.py
@@ -0,0 +1,43 @@
+# Copyright 2025 Canonical Ltd.
+# See LICENSE file for licensing details.
+#
+#
+import pytest
+import subprocess
+
+from charmed_kubeflow_chisme.rock import CheckRock
+
+@pytest.mark.abort_on_fail
+def test_rock():
+    """Test rock."""
+    check_rock = CheckRock("rockcraft.yaml")
+    rock_image = check_rock.get_name()
+    rock_version = check_rock.get_version()
+    LOCAL_ROCK_IMAGE = f"{rock_image}:{rock_version}"
+
+    subprocess.run(
+        [
+            "docker",
+            "run",
+            "--rm",
+            LOCAL_ROCK_IMAGE,
+            "exec",
+            "sh",
+            "-c",
+            "test -d /opt/conda/lib/R/library/tidyverse"
+        ],
+        check=True,
+    )
+    subprocess.run(
+        [
+            "docker",
+            "run",
+            "--rm",
+            LOCAL_ROCK_IMAGE,
+            "exec",
+            "sh",
+            "-c",
+            "test -f /opt/rsession.sh"
+        ],
+        check=True,
+    )
